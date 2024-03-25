6e24315acbf86459d126b1ef09b35d370623eb9b
user <sanakan.ver3@gmail.com>
2024-03-25 08:50:33 +0000

    "モデルとトークナイザーのパスを変更しました"

--- src/embedding_news_vis_mistral.py
+++ src/embedding_news_vis_mistral.py
@@ -35,10 +35,10 @@ def get_tsv_files_in_directory(directory):
 def generate_embeddings(text, model, tokenizer):
     max_length = 4096
     batch_dict = tokenizer(text, max_length=max_length - 1, return_attention_mask=False, padding=False, truncation=True)
-    batch_dict['input_ids'] = [[input_id] + [tokenizer.eos_token_id] for input_id in batch_dict['input_ids']]
-    batch_dict = tokenizer.pad(batch_dict, padding=True, return_attention_mask=True, return_tensors='pt')
+    batch_dict["input_ids"] = [[input_id] + [tokenizer.eos_token_id] for input_id in batch_dict["input_ids"]]
+    batch_dict = tokenizer.pad(batch_dict, padding=True, return_attention_mask=True, return_tensors="pt")
     outputs = model(**batch_dict)
-    embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
+    embeddings = last_token_pool(outputs.last_hidden_state, batch_dict["attention_mask"])
     embeddings = F.normalize(embeddings, p=2, dim=1)
     return embeddings.tolist()
 
@@ -49,8 +49,8 @@ def plot_embeddings(df):
     tooltips = ["ID: " + str(id_) + "<br>Title: " + title for id_, title in zip(id, title)]
     tsne = TSNE(n_components=2, random_state=0)
     tsne_results = tsne.fit_transform(embeddings)
-    fig = go.Figure(data=go.Scatter(x=tsne_results[:, 0], y=tsne_results[:, 1], mode='markers', text=tooltips, hoverinfo="text")) 
-    fig.update_layout(title='visualization of embeddings', xaxis=dict(title='t-SNE1'), yaxis=dict(title='t-SNE2'))
+    fig = go.Figure(data=go.Scatter(x=tsne_results[:, 0], y=tsne_results[:, 1], mode="markers", text=tooltips, hoverinfo="text")) 
+    fig.update_layout(title="visualization of embeddings", xaxis=dict(title="t-SNE1"), yaxis=dict(title="t-SNE2"))
     fig.show()
     fig.write_html("fig/tsne.html")
 
@@ -60,10 +60,10 @@ def main():
     combined_df = pd.concat([pd.read_csv(file, sep="\t", header=0) for file in tsv_files], axis=0)
     combined_df["ID"] = range(1, len(combined_df) + 1)
     combined_df.drop_duplicates(subset=["Title"], inplace=True)
-    tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-mistral-7b-instruct')
-    model = AutoModel.from_pretrained('intfloat/e5-mistral-7b-instruct')
-    combined_df['embeddings'] = combined_df['Text'].apply(lambda text: generate_embeddings(text, model, tokenizer))
-    print(len(combined_df['embeddings'].iloc[0]))
+    tokenizer = AutoTokenizer.from_pretrained("models/e5-mistral-7b-instruct")
+    model = AutoModel.from_pretrained("models/e5-mistral-7b-instruct")
+    combined_df["embeddings"] = combined_df["Text"].apply(lambda text: generate_embeddings(text, model, tokenizer))
+    print(len(combined_df["embeddings"].iloc[0]))
     plot_embeddings(combined_df)
 
 if __name__ == "__main__":
