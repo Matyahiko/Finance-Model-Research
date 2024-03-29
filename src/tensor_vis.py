fc6679446ee88c73fa398ad2f8b4f2e776802f97
user <sanakan.ver3@gmail.com>
2024-03-29 06:29:55 +0000

    テンソルをHTMLに変換する関数を追加しました

--- src/tensor_vis.py
+++ src/tensor_vis.py
@@ -1,24 +1,23 @@
 import numpy as np
-from sklearn.manifold import TSNE
-import plotly.graph_objects as go
 import webbrowser
+import os
 
-def plot_embeddings(embeddings):
-    tsne = TSNE(n_components=2, random_state=0)
-    tsne_results = tsne.fit_transform(embeddings)
-
-    fig = go.Figure(data=go.Scatter(x=tsne_results[:, 0], y=tsne_results[:, 1],
-                                    mode='markers'))
-    fig.update_layout(title='Visualization of Embeddings',
-                      xaxis=dict(title='t-SNE1'),
-                      yaxis=dict(title='t-SNE2'))
-
-    fig.write_html("embeddings_plot.html")
-    webbrowser.open("embeddings_plot.html")
+def tensor_to_html(tensor):
+    html = "<html><body><table>"
+    html += "<tr><th>Index</th><th>Value</th></tr>"
+    for i, value in enumerate(tensor):
+        html += f"<tr><td>{i}</td><td>{value}</td></tr>"
+    html += "</table></body></html>"
+    return html
 
 def main():
-    embeddings = np.load("log/random_news_tensor.npy", allow_pickle=True)
-    plot_embeddings(embeddings)
+    tensor = np.load("log/random_news_tensor.npy", allow_pickle=True)
+    html = tensor_to_html(tensor)
+
+    os.makedirs("log", exist_ok=True)  # ディレクトリが存在しない場合は作成
+    with open("log/tensor.html", "w") as f:
+        f.write(html)
+    webbrowser.open("file://" + os.path.abspath("log/tensor.html"))
 
 if __name__ == "__main__":
     main()
